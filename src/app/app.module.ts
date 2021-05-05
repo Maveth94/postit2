8530058c2141923a1af18fccc0253f668c92f82a
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-05 17:02:11 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
+import { ChuckService } from "./postit.service";
+import { HttpClientModule } from "@angular/common/http";
 import { ReadPostitComponent } from "./read-postit/read-postit.component";
 import { NewPostitComponent } from "./new-postit/new-postit.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    ReadPostitComponent,
-    NewPostitComponent
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [AppComponent, ReadPostitComponent, NewPostitComponent],
+  providers: [ChuckService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
