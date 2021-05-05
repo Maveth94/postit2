d7dde6cb971704e78d7ac980bf8fdc5a765cea7c
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-05 16:53:01 +0200

    1 file modified

--- src/app/postit.service.ts
+++ src/app/postit.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { LocalizedString } from "@angular/compiler";
 
 @Injectable({
   providedIn: "root"
 })
 export class ChuckService {
-  apiURLTot = "https://api.keyvalue.xyz/0e1f160e/myKey";
+  apiURLTot: string = "https://api.keyvalue.xyz/0e1f160e/myKey";
   apiURL: string = "https://api.keyvalue.xyz/";
   apiKey: string = "0e1f160e";
   apiEnd: string = "/myKey";
