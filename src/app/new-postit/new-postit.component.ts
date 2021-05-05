2f8faaa75d43f15f7a30f0de519e59a2cd9a6ce2
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-05 17:38:50 +0200

    2 files modified

--- src/app/new-postit/new-postit.component.ts
+++ src/app/new-postit/new-postit.component.ts
@@ -26,7 +26,7 @@ export class NewPostitComponent implements OnInit {
     //this.nMess = _mess;
     this.newPost.titolo = _tit;
     this.newPost.mess = _mess;
-    console.log("valore: " + typeof this.newPost.mess);
+    //console.log("valore: " + typeof this.newPost.mess);
     if (
       this.newPost.titolo !== (undefined || "" || " " || null) &&
       this.newPost.mess !== (undefined || "" || " " || null)
