980bcbc513f1d081a6d0b5d37277328f1acebfdb
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-06 10:40:40 +0200

    2 files modified

--- src/app/new-postit/new-postit.component.ts
+++ src/app/new-postit/new-postit.component.ts
@@ -11,29 +11,35 @@ export class NewPostitComponent implements OnInit {
   @Output() newPostEvent = new EventEmitter<postIt>();
   newPost: postIt;
 
+  /*
+
   nTit: string = "";
   nMess: string = "";
   nImp: boolean = false;
+  */
 
   conferma(nTit: string, nMess: string, nImp: string) {
     this.newPost = new postIt();
+    this.newPost.titolo = nTit;
+    this.newPost.mess = nMess;
     //ho dovuto fare un casr altrimenti l'elemento mi veniva passato come HTMLElement
     //quindi senza l'attributo value
-    this.nTit = (<HTMLInputElement>document.getElementById("nTit")).value;
+    //this.nTit = (<HTMLInputElement>document.getElementById("nTit")).value;
     //console.log("titolo: " + this.nTit);
-    this.nMess = (<HTMLInputElement>document.getElementById("nMess")).value;
+    //this.nMess = (<HTMLInputElement>document.getElementById("nMess")).value;
+    console.log(nImp);
     //console.log(<HTMLInputElement>document.getElementById("check").value);
     //this.nImp = (<HTMLInputElement>document.getElementById("check")).value;
-    this.newPost.titolo = this.nTit;
-    this.newPost.mess = this.nMess;
+    //this.newPost.titolo = this.nTit;
+    //this.newPost.mess = this.nMess;
     if (
       this.newPost.titolo !== ("" || " ") &&
       this.newPost.mess !== ("" || " ")
     ) {
       this.newPostEvent.emit(this.newPost);
       this.selezioneA = 0;
-      this.nTit = "";
-      this.nMess = "";
+      //this.nTit = "";
+      //this.nMess = "";
     } else {
       alert("Dati mancanti o  scorretti, ritenta");
       return;
