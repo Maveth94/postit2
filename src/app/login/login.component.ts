222f8e7f9fe341981a930d1130bd7b4cef064614
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-11 14:17:52 +0200

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 /**
-Far sì che se la pass è sbagliata non accedi, 
-far il pulsante indietro nel main in modo che si possa inserire una nuova chiave
+1) Far sì che se la pass è sbagliata non accedi, 
+2) far il pulsante indietro nel main in modo che si possa inserire una nuova chiave
  */
 
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
